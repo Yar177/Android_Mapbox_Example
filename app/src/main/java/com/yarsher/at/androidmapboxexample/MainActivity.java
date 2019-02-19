3448e8df792c839b12b78b8ab8de5d466d3aae68
Yar <yarsher.dev@gmail.com>
2019-02-19 11:03:15 -0500

    initLocationEngine

--- app/src/main/java/com/yarsher/at/androidmapboxexample/MainActivity.java
+++ app/src/main/java/com/yarsher/at/androidmapboxexample/MainActivity.java
@@ -5,11 +5,10 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.Toast;
 
-import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.*;
+import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -18,7 +17,9 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 
-public class MainActivity extends AppCompatActivity  {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener {
     private MapView mapView;
     private MapboxMap map;
     private PermissionsManager permissionsManager;
@@ -36,10 +37,47 @@ public class MainActivity extends AppCompatActivity  {
 
         mapView.onCreate(savedInstanceState);
 
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void initLocationEngine(){
+        locationEngine = new LocationEngineProvider(this).obtainBestLocationEngineAvailable();
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+        Location lastLocation = locationEngine.getLastLocation();
+        if (lastLocation != null){
+            originLocation = lastLocation;
+        }
 
 
+    }
 
+    private void initLocationLayer(){
 
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        map = mapboxMap;
+        enableLocation();
+
+    }
+
+   private void enableLocation(){
+        if (PermissionsManager.areLocationPermissionsGranted(this)){
+            initLocationEngine();
+            initLocationLayer();
+
+        }else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+
+   }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -77,4 +115,30 @@ public class MainActivity extends AppCompatActivity  {
         super.onDestroy();
         mapView.onDestroy();
     }
+
+    @Override
+    public void onConnected() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, "Location Permission needed tp show the maps", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted){
+            enableLocation();
+        }
+
+    }
+
+
 }
