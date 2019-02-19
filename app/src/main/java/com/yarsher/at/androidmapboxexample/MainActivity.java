309c97e9095745b1d1b2c0ac00f7c99510496147
Yar <yarsher.dev@gmail.com>
2019-02-19 12:13:52 -0500

    getRoute

--- app/src/main/java/com/yarsher/at/androidmapboxexample/MainActivity.java
+++ app/src/main/java/com/yarsher/at/androidmapboxexample/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,6 +13,8 @@ import android.widget.Toast;
 import com.mapbox.android.core.location.*;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
@@ -25,8 +28,14 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener, MapboxMap.OnMapClickListener {
     private MapView mapView;
@@ -40,6 +49,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Marker destMarker;
 
     private Button startButton;
+
+    private NavigationMapRoute navigationMapRoute;
+
+    private static final String TAG = "Main Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -207,14 +221,48 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         destMarker = map.addMarker(new MarkerOptions().position(point));
-
         destinationPosition = Point.fromLngLat(point.getLongitude(), point.getLatitude());
         originPosition = Point.fromLngLat(originLocation.getLongitude(), originLocation.getLatitude());
-
+        getRoute(originPosition, destinationPosition);
         startButton.setEnabled(true);
         startButton.setBackgroundResource(R.color.mapbox_blue);
 
+    }
+
+
+    private void getRoute(Point origin, Point dest){
+        NavigationRoute.builder()
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(dest)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        if (response.body() == null) {
+                            Log.e(TAG, " no routes found check token");
+                            return;
+                        }else if (response.body().routes().size() == 0){
+                            Log.e(TAG, "No routes found");
+                           return;
+                        }
+                        DirectionsRoute currentRoute = response.body().routes().get(0);
+                        if (navigationMapRoute != null){
+                            navigationMapRoute.removeRoute();
+                        }else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, map);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                        Log.i(TAG, "Error : " + t.getMessage());
+                    }
+                });
 
 
     }
+
+
 }
